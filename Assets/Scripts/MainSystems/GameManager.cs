62b36e2a169f4f1e7974d2af73517f92c594640e
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-11-26 17:16:46 +0900

    戦闘システム作ってる

---
+++ Assets/Scripts/MainSystems/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = default;
+
+    void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = new GameManager();
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/QTE.cs
+++ Assets/Scripts/QTE.cs
@@ -5,18 +5,9 @@ using UnityEngine.UI;
 
 public class QTE : MonoBehaviour
 {
-
-    [SerializeField]float m_yuuyo = 1.8f;
-        
-    // Start is called before the first frame update
-    void Start()
+    
+    public void QTEBited()
     {
-        StartCoroutine(QTEBited());
-    }
-
-    IEnumerator QTEBited()
-    {
-        yield return new WaitForSeconds(m_yuuyo);
         Player._pHP -= 10;
         Destroy(this.gameObject);
     }
