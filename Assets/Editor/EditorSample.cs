b8b83d82f33ea9e456a4971a498bcceb66766a57
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-12-04 15:31:01 +0900

    エディター拡張勉強中

--- Assets/Editor/EditorSample.cs
+++ Assets/Editor/EditorSample.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEditor;
 
 public class EditorSample : EditorWindow
 {
 
     SOSample _sample;
+    private const string ASSET_PATH = "Assets/Resources/SOSample.asset";
 
     [MenuItem("Editor/Sample")]
 
@@ -21,9 +23,93 @@ public class EditorSample : EditorWindow
             _sample = ScriptableObject.CreateInstance<SOSample>();
         }
 
-        using (new GUILayout.HorizontalScope())
+        Color defaultColor = GUI.backgroundColor;
+        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
         {
+            GUI.backgroundColor = Color.gray;
+            using (new GUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                GUILayout.Label("設定");
+            }
+            GUI.backgroundColor = defaultColor;
+
             _sample.SampleIntValue = EditorGUILayout.IntField("サンプルint", _sample.SampleIntValue);
         }
+        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+        {
+            GUI.backgroundColor = Color.gray;
+            using (new GUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                GUILayout.Label("ファイル操作");
+            }
+            GUI.backgroundColor = defaultColor;
+
+            GUILayout.Label("パス：" + ASSET_PATH);
+
+            using (new GUILayout.HorizontalScope(GUI.skin.box))
+            {
+                GUI.backgroundColor = Color.green;
+                // 読み込みボタン
+                if (GUILayout.Button("読み込み"))
+                {
+                    Import();
+                }
+                GUI.backgroundColor = Color.magenta;
+                // 書き込みボタン
+                if (GUILayout.Button("書き込み"))
+                {
+                    Export();
+                }
+                GUI.backgroundColor = defaultColor;
+            }
+        }
+    }
+    private void Export()
+    {
+        // 読み込み
+        SOSample sample = AssetDatabase.LoadAssetAtPath<SOSample>(ASSET_PATH);
+        if (sample == null)
+        {
+            sample = ScriptableObject.CreateInstance<SOSample>();
+        }
+
+        // 新規の場合は作成
+        if (!AssetDatabase.Contains(sample as UnityEngine.Object))
+        {
+            string directory = Path.GetDirectoryName(ASSET_PATH);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            // アセット作成
+            AssetDatabase.CreateAsset(sample, ASSET_PATH);
+        }
+
+        // コピー
+        EditorUtility.CopySerialized(_sample, sample);
+
+        // 直接編集できないようにする
+        sample.hideFlags = HideFlags.NotEditable;
+        // 更新通知
+        EditorUtility.SetDirty(sample);
+        // 保存
+        AssetDatabase.SaveAssets();
+        // エディタを最新の状態にする
+        AssetDatabase.Refresh();
+    }
+
+    private void Import()
+    {
+        if (_sample == null)
+        {
+            _sample = ScriptableObject.CreateInstance<SOSample>();
+        }
+
+        SOSample sample = AssetDatabase.LoadAssetAtPath<SOSample>(ASSET_PATH);
+        if (sample == null)
+            return;
+
+        // コピーする
+        EditorUtility.CopySerialized(sample, _sample);
     }
 }
---
+++ Assets/Editor/ScriptableObjectSampleLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScriptableObjectSampleLoader : MonoBehaviour
+{
+    private SOSample _sample;
+
+    private void Start()
+    {
+        // 読み込み & 生成（ファイル名はASSET_PATHで定義したもの）
+        _sample = ScriptableObject.Instantiate(Resources.Load<SOSample>("SOtSample"));
+    }
+}
