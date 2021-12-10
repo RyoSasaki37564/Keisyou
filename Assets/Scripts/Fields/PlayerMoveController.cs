e2b8d85397554b1745d49aeb30062faffbc43dcb
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-12-10 17:27:18 +0900

    フィールドでの移動基礎完成

---
+++ Assets/Scripts/Fields/PlayerMoveController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ターンベースでのプレイヤー移動を制御するコンポーネント
+/// </summary>
+[RequireComponent(typeof(GridMoveController))]
+public class PlayerMoveController : MonoBehaviour
+{
+    /// <summary>１ターンで動くのにかける時間（単位: 秒）</summary>
+    [SerializeField] float m_moveTime = 1f;
+    GridMoveController m_gridMove = null;
+
+    void Start()
+    {
+        m_gridMove = GetComponent<GridMoveController>();
+
+    }
+
+    void Update()
+    {
+        if (m_gridMove.IsMoving)    // 動いている最中は何もしない
+        {
+            return;
+        }
+
+        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+        if (h != 0 || v != 0)
+        {
+            m_gridMove.Move((int)h, (int)v, m_moveTime);
+            /*
+            // 移動可能ならば移動して、ターンを進める
+            if (m_gridMove.Move((int)h, (int)v, m_moveTime))
+            {
+            }
+            */
+        }
+        
+    }
+}
