38364acb10bcaed8d7378d95fa23a4018e8b723f
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-01-15 18:37:38 +0900

    いらん関数消した

--- Assets/Scripts/NineKeyInput.cs
+++ Assets/Scripts/NineKeyInput.cs
@@ -18,16 +18,8 @@ public class NineKeyInput : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit.collider)
             {
-                Debug.Log(hit.collider.gameObject.name + " " + GetAngle(hit.collider.gameObject.transform.position, hit.normal));
+                Debug.Log(hit.collider.gameObject.name);
             }
         }
-    }
-    float GetAngle(Vector2 start, Vector2 target)
-    {
-        Vector2 dt = target - start;
-        float rad = Mathf.Atan2(dt.y, dt.x);
-        float degree = rad * Mathf.Rad2Deg;
-
-        return degree;
     }
 }
