12bbb541649af031ef1ed89e8102996f1854b69b
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-01-15 19:26:27 +0900

    龍撃コマンド入力システム開発開始

--- Assets/Scripts/NineKeyInput.cs
+++ Assets/Scripts/NineKeyInput.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class NineKeyInput : MonoBehaviour
 {
+    [SerializeField] GameObject[] m_nines = new GameObject[9];
+
+    public struct CommandCode
+    {
+        public int number { get; set; }
+        public int contact { get; set; }
+    }
+    List<CommandCode> m_commandList = new List<CommandCode>();
 
     // Start is called before the first frame update
     void Start()
     {
 
     }
+
     void Update()
     {
         if (Input.GetButton("Fire1"))
@@ -19,6 +28,7 @@ public class NineKeyInput : MonoBehaviour
             if (hit.collider)
             {
                 Debug.Log(hit.collider.gameObject.transform.parent.name + " " + hit.collider.gameObject.name);
+
             }
         }
     }
