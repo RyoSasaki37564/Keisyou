af805c00f54e14b4357ffd27ec5fbcd127c48e47
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-01-17 16:31:22 +0900

    角度式の入力にも挑戦

---
+++ Assets/Scripts/ContactReset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactReset : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_commands = new GameObject[9];
+
+    public void Resetter()
+    {
+        foreach(var i in m_commands)
+        {
+            i.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/NineKeyInput.cs
+++ Assets/Scripts/NineKeyInput.cs
@@ -6,17 +6,26 @@ public class NineKeyInput : MonoBehaviour
 {
     [SerializeField] GameObject[] m_nines = new GameObject[9];
 
+    Vector3 m_mousePosDelta;
+
     public struct CommandCode
     {
         public int number { get; set; }
         public int contact { get; set; }
+
+        public CommandCode(int numID, int conID)
+        {
+            number = numID;
+            contact = conID;
+        }
     }
     List<CommandCode> m_commandList = new List<CommandCode>();
+    int m_indexer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_indexer = 0;
     }
 
     void Update()
@@ -27,9 +36,28 @@ public class NineKeyInput : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit.collider)
             {
-                Debug.Log(hit.collider.gameObject.transform.parent.name + " " + hit.collider.gameObject.name);
+                Debug.Log(GetAim(m_mousePosDelta, Input.mousePosition));
 
+                Debug.Log("接触情報：" + hit.collider.gameObject.transform.parent.name + " " + hit.collider.gameObject.name);
+
+                CommandCode m_CC = new CommandCode(int.Parse(hit.collider.gameObject.transform.parent.name), 
+                    int.Parse(hit.collider.gameObject.name));
+                hit.collider.gameObject.transform.parent.gameObject.SetActive(false);
+                m_commandList.Add(m_CC);
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        m_mousePosDelta = Input.mousePosition;
+    }
+
+    public float GetAim(Vector3 p1, Vector3 p2)
+    {
+        float dx = p2.x - p1.x;
+        float dy = p2.y - p1.y;
+        float rad = Mathf.Atan2(dy, dx);
+        return rad * Mathf.Rad2Deg;
     }
 }
