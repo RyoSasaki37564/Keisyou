781bb1621ca844cae5d35586eec3cc1dc61119ea
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-02-03 02:17:13 +0900

    スピード線は作れそう

--- Assets/Scripts/Effects/EffecterTest.cs
+++ Assets/Scripts/Effects/EffecterTest.cs
@@ -6,12 +6,6 @@ public class EffecterTest : MonoBehaviour
 {
     [SerializeField] float m_sp = 0.01f; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
