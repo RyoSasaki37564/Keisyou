ab0eb613ade6fcc83d2a197c0911c472a0670eb2
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-02-03 06:40:11 +0900

    スピード線エフェクター作った。あとカットインも

---
+++ Assets/Scripts/Effects/LineEffectMnager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineEffectMnager : MonoBehaviour
+{
+    [SerializeField] GameObject m_speedLine = default; //トレイルがついてるオブジェクト。線書くぜ
+
+    [SerializeField] int m_lineCount = 100;
+
+    int m_indexer = 0;
+
+    [SerializeField] GameObject m_maxHeight;
+    [SerializeField] GameObject m_minHeight;
+
+    [SerializeField] float m_interval = 0.05f;
+
+    float rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // m_lineCount の数だけ線をプールする
+        for (var i = 0; i < m_lineCount; i++)
+        {
+            var x = Instantiate(m_speedLine);
+            x.transform.SetParent(this.gameObject.transform);
+            x.SetActive(false);
+        }
+        Liner();
+    }
+
+    void Liner()
+    {
+        rand = Random.Range(m_minHeight.transform.position.y, m_maxHeight.transform.position.y);
+        StartCoroutine(SpeedLiner());
+    }
+
+    IEnumerator SpeedLiner()
+    {
+        yield return new WaitForSeconds(m_interval);
+        var x = this.gameObject.transform.GetChild(m_indexer).gameObject;
+        x.transform.position = new Vector3(this.transform.position.x, rand, this.transform.position.z);
+        x.SetActive(true);
+        m_indexer++;
+        if (m_indexer == m_lineCount)
+        {
+            m_indexer = 0;
+        }
+        Liner();
+    }
+}
---
+++ Assets/Scripts/Effects/LineMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMover : MonoBehaviour
+{
+    [SerializeField] float m_sp = 0.01f;
+
+    [SerializeField] GameObject m_goal = default;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(m_sp, 0, 0);
+
+        if(this.transform.position.x > m_goal.transform.position.x)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
