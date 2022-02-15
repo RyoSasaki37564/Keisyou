e242781fadd5eebf64b857b5b5f8f53518112bfe
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-02-16 00:08:46 +0900

    超簡単な戦闘開始演出つくった

---
+++ Assets/Scripts/StandUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandUp : MonoBehaviour
+{
+    [SerializeField] GameObject m_UICanvas = default;
+
+    [SerializeField] SEPlay m_SE = default;
+
+    [SerializeField] GameObject m_parent = default;
+
+    public void BattleStart()
+    {
+        m_SE.MyPlayOneShot();
+        m_UICanvas.SetActive(true);
+        m_parent.SetActive(false);
+    }
+}
