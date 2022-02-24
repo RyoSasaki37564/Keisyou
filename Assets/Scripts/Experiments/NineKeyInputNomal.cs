b1a11d29844e7bbf71fba7a19d904552b3d7ebe5
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-02-24 17:37:46 +0900

    「突き」のひな型が完成

--- Assets/Scripts/Experiments/NineKeyInputNomal.cs
+++ Assets/Scripts/Experiments/NineKeyInputNomal.cs
@@ -10,6 +10,8 @@ public class NineKeyInputNomal : MonoBehaviour
 
     bool m_isIn = false;
 
+    bool m_slustFlg = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,7 @@ public class NineKeyInputNomal : MonoBehaviour
             {
                 m_thisCol.enabled = false;
                 m_isIn = true;
+                m_slustFlg = true;
             }
         };
 
@@ -41,6 +44,14 @@ public class NineKeyInputNomal : MonoBehaviour
         {
             m_thisCol.enabled = true;
             m_isIn = false;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit.collider == m_thisCol && m_slustFlg == true)
+            {
+                Debug.Log(5);
+            }
+            m_slustFlg = false;
         };
     }
 
@@ -56,8 +67,12 @@ public class NineKeyInputNomal : MonoBehaviour
         {
             m_isIn = false;
             m_mousePosDelta = Input.mousePosition;
-            Debug.Log(m_mousePosDelta);
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        m_slustFlg = false;
     }
 
 }
