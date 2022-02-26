ff09e9ad8f4d15ce1438e0a3812db97e9c9747d9
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-02-26 12:54:33 +0900

    とりあえず動作は固めた。あとはID化と斬撃刺突マークの表示

--- Assets/Scripts/Experiments/NineKeyInputNomal.cs
+++ Assets/Scripts/Experiments/NineKeyInputNomal.cs
@@ -17,6 +17,8 @@ public class NineKeyInputNomal : MonoBehaviour
 
     float m_zangekiDirection; //斬撃角度
 
+    const float c_originDir = 22.5f; //斬撃方向IDをとるための原角度
+
     // Start is called before the first frame update
     void Start()
     {
@@ -61,11 +63,7 @@ public class NineKeyInputNomal : MonoBehaviour
             }
             else if (m_zangekiFlg == true)
             {
-                var heading = Input.mousePosition - m_mousePosDelta;
-                var distance = heading.magnitude;
-                var direction = heading / distance;
-                m_zangekiDirection = Mathf.Atan2(direction.y, direction.x) * (180 / Mathf.PI);
-                Debug.Log(Mathf.Repeat(m_zangekiDirection, 360) + "うち");
+                Debug.Log(ZangekiDirection() + "うち");
             }
             m_slustFlg = false;
 
@@ -83,7 +81,6 @@ public class NineKeyInputNomal : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && m_isIn == true)
         {
-            Debug.LogWarning("入ったー");
             m_isIn = false;
             m_mousePosDelta = Input.mousePosition;
             m_zangekiFlg = true;
@@ -94,11 +91,7 @@ public class NineKeyInputNomal : MonoBehaviour
     {
         if (m_zangekiFlg == true)
         {
-            var heading = Input.mousePosition - m_mousePosDelta;
-            var distance = heading.magnitude;
-            var direction = heading / distance;
-            m_zangekiDirection = Mathf.Atan2(direction.y, direction.x) * (180 / Mathf.PI);
-            Debug.Log(Mathf.Repeat(m_zangekiDirection, 360) + "ぬけ");
+            Debug.Log(ZangekiDirection() + "ぬけ");
 
             m_zangekiFlg = false;
 
@@ -110,13 +103,60 @@ public class NineKeyInputNomal : MonoBehaviour
         }
     }
 
-    /*
     int ZangekiDirection()
     {
         var heading = Input.mousePosition - m_mousePosDelta;
         var distance = heading.magnitude;
         var direction = heading / distance;
         m_zangekiDirection = Mathf.Repeat(Mathf.Atan2(direction.y, direction.x) * (180 / Mathf.PI), 360);
-        if()
-    }*/
+
+        //22.5　は　45 の半分。45は　360 を斬撃の方向数「8」で割った数(5 は突きとして扱う)
+        
+        //ゲーム画面では 九字鍵は
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+        //　と並んでいるが、この処理部分では円形角度計算を行うため少しでもif文の可読性を上げるために
+        //　7,8,9,6,3,2,1,elseで4　という風にぐるりと円形イメージで記述を行う
+
+        if (c_originDir <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45) 
+        {
+            return 7;
+        }
+        else if(c_originDir + 45 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 2)
+        {
+            return 8;
+        }
+        else if (c_originDir + 45 * 2 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 3)
+        {
+            return 9;
+        }
+        else if (c_originDir + 45 * 3 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 4)
+        {
+            return 6;
+        }
+        else if (c_originDir + 45 * 4 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 5)
+        {
+            return 3;
+        }
+        else if (c_originDir + 45 * 5 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 6)
+        {
+            return 2;
+        }
+        else if (c_originDir + 45 * 6 <= m_zangekiDirection
+            && m_zangekiDirection < c_originDir + 45 * 7)
+        {
+            return 1;
+        }
+        else
+        {
+            return 4;
+        }
+    }
 }
