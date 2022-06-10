487ba0a80b57bbe8485760dd2bd3c5933fe02b10
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-06-10 20:03:07 +0900

    kuesutotukuttoruwai

--- Assets/Scripts/MainSystems/Quest/QuestBase.cs
+++ Assets/Scripts/MainSystems/Quest/QuestBase.cs
@@ -1,8 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class QuestBase
+﻿
+public abstract class QuestBase
 {
+    protected string m_name;
 
+    protected string m_setumei;
+
+    protected bool[] m_shinkoudoFlgs;
+
+    protected int m_counter = 0; //クエスト進行度
+
+    public bool m_endFlg;
+
+    protected QuestBase(string name, string setumei, int flgCount)
+    {
+        m_name = name;
+        m_setumei = setumei;
+        m_shinkoudoFlgs = new bool[flgCount];
+    }
+
+    public abstract void QStateAdvance();
 }
---
+++ Assets/Scripts/MainSystems/Quest/TestQuest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestQuest : QuestBase
+{
+    TestQuest m_instanse = new TestQuest("テスト", "テストの説明", 3);
+
+    public TestQuest(string name, string setumei, int flgCount) : base(name, setumei, flgCount)
+    {
+        m_name = name;
+        m_setumei = setumei;
+        m_shinkoudoFlgs = new bool[flgCount];
+    }
+
+    public override void QStateAdvance()
+    {
+        switch(m_counter)
+        {
+            case 0:
+                m_setumei = "クエストスタート";
+                m_counter++;
+                break;
+            case 1:
+                break;
+            case 2:
+                m_setumei = "クエスト完了";
+                break;
+        }
+    }
+}
