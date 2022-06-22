6b70c2be672a5c71cc35337e841496fde7cb05f4
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-06-22 09:00:03 +0900

    シナリオシステムの雛ができた

--- Assets/Scripts/MainSystems/ScenarioTexts/CustomPlayableBehaviour.cs
+++ Assets/Scripts/MainSystems/ScenarioTexts/CustomPlayableBehaviour.cs
@@ -8,15 +8,15 @@ using UnityEngine.Playables;
 public class CustomPlayableBehaviour : PlayableBehaviour
 {
     private PlayableDirector director;
-    public GameObject templateGameObject;
+    [System.NonSerialized] public GameObject templateGameObject;
 
     [SerializeField] float m_updateSeconds = 0.2f; //表示する文字を更新するスパンの秒数
     float m_time;
 
+    Text m_scenarioText;
+    Text m_speakerNameText;
     [SerializeField] string m_name = "しゃべってるやつの名前";
     [SerializeField, TextArea(1, 3)] string m_scenario = "龍滅の刃...?\n" + "龍滅の刃...!?";
-    Text m_speakerNameText;
-    Text m_scenarioText;
     int m_openedCount = 1; //表示文字数
 
     public override void OnPlayableCreate(Playable playable)
@@ -64,7 +64,7 @@ public class CustomPlayableBehaviour : PlayableBehaviour
             if (s[s.Length - 1] == '\n')
             {
                 //改行に際して2秒遅くする
-                if (m_time == m_updateSeconds + 2)
+                if (m_time >= m_updateSeconds + 2)
                 {
                     m_scenarioText.text = s;
                     if (s != m_scenario)
@@ -76,7 +76,7 @@ public class CustomPlayableBehaviour : PlayableBehaviour
             }
             else
             {
-                if (m_time == m_updateSeconds)
+                if (m_time >= m_updateSeconds)
                 {
                     m_scenarioText.text = s;
                     if (s != m_scenario)
